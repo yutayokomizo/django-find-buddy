dd8cf71041805e9fc427700ec6020f6790f48ac2
yutayokomizo <yutayoko.prime@gmail.com>
2022-08-17 15:12:20 +0900

    Update for media

--- studybud/settings.py
+++ studybud/settings.py
@@ -134,7 +134,7 @@ MEDIA_URL = "/images/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
-MEDIA_ROOT = BASE_DIR / "images"
+MEDIA_ROOT = BASE_DIR / "static/images"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- studybud/urls.py
+++ studybud/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
 from django.conf import settings
-from django.conf.urls.static import static
+
+# from django.conf.urls.static import static
+from django.views.static import serve
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("base.urls")),
+    url(r"^images/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
