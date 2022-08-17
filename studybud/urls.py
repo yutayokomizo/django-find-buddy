79e96cd524b17d4f7ec863b83d30f70f7809b7a8
yutayokomizo <yutayoko.prime@gmail.com>
2022-08-17 15:16:01 +0900

    Update media path

--- studybud/urls.py
+++ studybud/urls.py
@@ -9,7 +9,7 @@ from django.views.static import serve
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("base.urls")),
-    url(r"^images/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    path(r"^images/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
 ]
 
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
