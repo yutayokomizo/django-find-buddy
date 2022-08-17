9dd052b8423839fca2b47dbe583d1d97ed24eb64
yutayokomizo <yutayoko.prime@gmail.com>
2022-08-17 16:30:53 +0900

    Configured for S3

--- studybud/settings.py
+++ studybud/settings.py
@@ -150,3 +150,10 @@ DATABASES["default"].update(db_from_env)
 # Simplified static file serving.
 # https://pypi.org/project/whitenoise/
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+AWS_S3_ACCESS_KEY_ID = os.environ.get("AWS_S3_ACCESS_KEY_ID")
+AWS_S3_SECRET_ACCESS_KEY = os.environ.get("AWS_S3_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = "django-find-buddy-avatar"
+AWS_QUERYSTRING_AUTH = False
