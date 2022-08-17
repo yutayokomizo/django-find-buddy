bd58fe11269cb9821034788a26dfd41bbae8b99d
yutayokomizo <yutayoko.prime@gmail.com>
2022-08-17 14:12:50 +0900

    Delete experimental api

--- base/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.getRoutes),
-    path('rooms/', views.getRooms),
-    path('room/<str:pk>/', views.getRoom)
-]
--- studybud/settings.py
+++ studybud/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "base.apps.BaseConfig",
-    "rest_framework",
     "corsheaders",
 ]
 
@@ -141,8 +140,6 @@ MEDIA_ROOT = BASE_DIR / "static/images"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-CORS_ALLOW_ALL_ORIGINS = True
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
